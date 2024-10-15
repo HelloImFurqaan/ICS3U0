0e27a69219c81a0215b8365535f3700b11af58c8
HelloImFurqaan <752879@pdsb.net>
2024-10-15 10:02:19 -0400

    Update Lesson1.py

--- U2InpOut/Lesson1.py
+++ U2InpOut/Lesson1.py
@@ -3,3 +3,10 @@ name = "Leslie"
 print("Hello", name)
 
 # Prediction: I belive this code will output Hello Leslie
+# Inspection: The code ran as I had predicted. Since the comma in the print command was not in the quotations, it was not printed. 
+# To get a comma in the output you would need to place another comma inside the string.
+
+name = "Furqaan"
+print("Hello", name)
+
+# Variables hold input values. The variable name in this case holds a string with my name; Furqaan.

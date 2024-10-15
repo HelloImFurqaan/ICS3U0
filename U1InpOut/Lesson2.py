de5509f6f74ce43c0c14a6471a1e6a35ad39c0e5
HelloImFurqaan <752879@pdsb.net>
2024-10-15 10:32:19 -0400

    Create Lesson2.py

---
+++ U1InpOut/Lesson2.py
@@ -0,0 +1,30 @@
+print(24)
+
+# Predict: I think this will output the number 24.
+# Inspect: The code ran exactly as I thought, since the integer 24 was entered into the print command directly.
+
+amount = 24
+print("Two dozens is: ", amount)
+
+# The use of a variable is an improvement because it allows for better readability
+
+amount = 7 * 4
+print("forty-two is: ", amount)
+
+# Corrected output:
+
+print("Twenty-eight is: ", amount)
+
+# Modify 3:
+amount = 8
+amount = amount + 1
+print("The amount is: ", amount)
+# This statement is adding + 1 to the variable amount. amount = 8, amount + 1 = 8 + 1 = 9.
+# The syntax += works in the exact same way, with more simplicity.
+
+# Modify 4:
+amount = 8
+amount = amount * 2
+print(amount)
+# This statement is multiplying the variable amount by 2. amount = 8, amount * 2 = 8 * 2 = 16.
+# The syntax *= works in the exact same way, with more simplicity.

f3e3970a8ad0928a102fc7c7316305c43b2d5c8b
HelloImFurqaan <752879@pdsb.net>
2024-10-16 01:00:51 -0400

    Update and rename Lesson3 to Lesson3.py

---
+++ U1InpOut/Lesson3.py
@@ -0,0 +1,27 @@
+print("Welcome to Sanjay's Koala Party!")
+print("It's possible Sanjay can distribute ", 12)
+print(" cookies for each bowl.")
+
+# Predict;
+# I believe the output will be:
+# Welcome to Sanjay's Koala Party!
+# It's possible Sanjay can distribute  12
+#  cookies for each bowl.
+
+# Run;
+# It ran as predicted.
+
+# Corrected code, fix spacing around 12:
+print("Welcome to Sanjay's Koala Party!")
+print("It's possible Sanjay can distribute", 12)
+print("cookies for each bowl.")
+
+# Task B:
+print("Sanjay has", 6, "bowls for", 144, "cookies.")
+
+# Task C:
+cookies = 144
+bowls = 6
+num = cookies/bowls
+print("The number of cookies per bowl is: %d" % num)
+# There is one text value and 3 number values

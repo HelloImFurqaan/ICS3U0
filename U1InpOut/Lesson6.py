78ce26540f9cb2c5e5e319ec423f8378be396b05
HelloImFurqaan <752879@pdsb.net>
2024-10-23 02:24:50 -0400

    Create Lesson6.py

---
+++ U1InpOut/Lesson6.py
@@ -0,0 +1,10 @@
+by = int(input("Please enter your birth year: "))
+age = int(input("Please enter your age: "))
+by = by*2
+by = by + 5
+by = by*50
+by = by + age
+by = by - 250
+by = by/100
+print(by)
+print("The number above represents your birth year, with the decimal of your age "

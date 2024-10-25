f52d18296caf16a68d2657d093bede65617962d0
HelloImFurqaan <752879@pdsb.net>
2024-10-25 10:19:26 -0400

    Update U3L1.py

--- 3 Arrays&Functions/U3L1.py
+++ 3 Arrays&Functions/U3L1.py
@@ -1,8 +1,6 @@
-s = ""
 num = 0
 progname = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in."
 for c in progname:
-    s = c
-    if (s == " "):
+    if (c == " "):
       num += 1
 print ("There are", num, "spaces in the text.")

ffa1d4a84648bb22a89687162cf3f4fadc56f980
HelloImFurqaan <752879@pdsb.net>
2024-10-25 10:03:49 -0400

    Create U3L1.py

---
+++ 3 Arrays&Functions/U3L1.py
@@ -0,0 +1,8 @@
+s = ""
+num = 0
+progname = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in."
+for c in progname:
+    s = c
+    if (s == " "):
+      num += 1
+print ("There are", num, "spaces in the text.")
